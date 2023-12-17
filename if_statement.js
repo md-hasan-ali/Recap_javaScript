4566c5c1e4e8345bc639b6c06a6708408819f191
md-hasan-ali <hasanaliringku@gmail.com>
2023-12-17 11:15:39 +0600

    if else statement

---
+++ if_statement.js
@@ -0,0 +1,13 @@
+let a = 60;
+let b = 50;
+let c = 30;
+
+if (a > b && a > c) {
+    console.log("A is big number")
+}
+else if (b > a && b > c) {
+    console.log("B is big number")
+}
+else {
+    console.log("C is big number")
+}
