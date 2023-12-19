d3a3c79a7323e1c1f43764f7367372f01b0a76a6
md-hasan-ali <hasanaliringku@gmail.com>
2023-12-19 21:40:07 +0600

    Function with Bind method

--- function.js
+++ function.js
@@ -15,4 +15,18 @@ let max = Math.max.apply(null, array)
 let array1 = [1,2,3,4];  
 let newarray=[5,6,7,8,10,20]  
 array.push.apply(array1, newarray);  
-console.log(array1)
+// console.log(array1)
+
+// Example of using the Bind method
+
+let website = {  
+    name: "Hasan Ali",  
+    getName: function() {  
+      return this.name;  
+    }  
+  }  
+
+  let unboundGetName = website.getName;  
+  console.log(unboundGetName)
+  let boundGetName = unboundGetName.bind(website);  
+  console.log(boundGetName());  
