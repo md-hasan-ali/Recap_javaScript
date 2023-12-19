c1ce2a5b5458dc4331cdbecf4ca43e2a2040dcbb
md-hasan-ali <hasanaliringku@gmail.com>
2023-12-19 18:54:13 +0600

    recap funtion with apply method

---
+++ function.js
@@ -0,0 +1,18 @@
+// Function with a rest parameter to handle variable arguments
+function sum(...numbers) {
+    return numbers.reduce((acc, num) => acc + num, 0);
+}
+let total = sum(1, 2, 3, 4, 5, 10); // Output: 25
+// console.log(total)
+
+
+// Example of using the apply method
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 52, 36, 85, 20, 30, 10]
+let max = Math.max.apply(null, array)
+// console.log(max)
+
+let array1 = [1,2,3,4];  
+let newarray=[5,6,7,8,10,20]  
+array.push.apply(array1, newarray);  
+console.log(array1)
