7c1a17f6f3e921fd10ae48ee9c5de104de9bbabf
md-hasan-ali <hasanaliringku@gmail.com>
2023-12-25 21:01:29 +0600

    JS Constructor

--- OOP/class.js
+++ OOP/class.js
@@ -12,10 +12,43 @@ class Animal {
 let cat = new Animal('Cat', 'Meow');
 let dog = new Animal('Dog', 'Woof');
 
-cat.makeSound();
-dog.makeSound(); 
+// cat.makeSound();
+// dog.makeSound();
 
 
+class Employee extends Animal {
+    constructor(name, sound) {
+        super(name, sound)
+    }
+}
+var emp = new Employee("Abul", "Manuser moto");
+console.log("Name", emp.name + ", " + emp.sound);
+
+
+
+// Inheritance with classes
+// class Bird extends Animal {
+//     constructor(name, sound, canFly) {
+//         super(name, sound);
+//         this.canFly = canFly;
+//     }
+//     fly() {
+//         if (this.canFly) {
+//             console.log(`${this.name} is flying.`);
+//         } else {
+//             console.log(`${this.name} cannot fly.`);
+//         }
+//     }
+// }
+// // Creating instances of the Bird class
+// let sparrow = new Bird('Sparrow', 'Chirp', true);
+// let penguin = new Bird('Penguin', 'Honk', false);
+
+// sparrow.makeSound();
+// sparrow.fly();
+
+// penguin.makeSound();
+// penguin.fly();
 
 
 
