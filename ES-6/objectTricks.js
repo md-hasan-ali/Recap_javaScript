8d767f1a04db65cc258fda473bd9902fec7d755f
md-hasan-ali <hasanaliringku@gmail.com>
2024-01-07 15:06:35 +0600

    ES6 Object tricks

---
+++ ES-6/objectTricks.js
@@ -0,0 +1,13 @@
+const language = {
+    lang: "JavaScript",
+    use: "Web application",
+    year: 1995
+}
+// console.log(Object.keys(language))
+// console.log(Object.values(language))
+// console.log(Object.entries(language))
+
+
+// Adding a new property and value 
+language.shortName = "JS";
+console.log(language)
