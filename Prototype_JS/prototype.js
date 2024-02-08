049777ca83e107eac36f48272898cf5ebb5d4a44
md-hasan-ali <hasanaliringku@gmail.com>
2024-02-08 14:24:12 +0600

    Use new keyword in prototype

--- Prototype_JS/prototype.js
+++ Prototype_JS/prototype.js
@@ -1,5 +1,4 @@
-// Prototype 
-
+// Basic Prototype 
 function Person(name, age) {
     let person = Object.create(Person.prototype)
     person.name = name;
@@ -15,5 +14,26 @@ Person.prototype = {
     }
  }
 
-const hasan = Person("Hasan", 25);
-hasan.sleep()
+// const hasan = Person("Hasan", 25);
+// hasan.sleep()
+
+
+// New keyword 
+
+function Person1(name, age) {
+    // let person = Object.create(Person.prototype)
+    this.name = name;
+    this.age= age;
+    // return person;
+}
+Person1.prototype = {
+    eat() {
+        console.log("Person is eating...")
+    },
+    sleep() {
+        console.log("Person is sleeping...")
+    }
+ }
+
+const hasan1 = new Person1("Hasan", 25);
+// hasan1.sleep()
