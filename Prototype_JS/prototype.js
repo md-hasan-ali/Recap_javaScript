665d61c2dcbd96968e68a3056d7dc7ba67982740
md-hasan-ali <hasanaliringku@gmail.com>
2024-02-08 14:42:30 +0600

    using class in prototype_js

--- Prototype_JS/prototype.js
+++ Prototype_JS/prototype.js
@@ -2,7 +2,7 @@
 function Person(name, age) {
     let person = Object.create(Person.prototype)
     person.name = name;
-    person.age= age;
+    person.age = age;
     return person;
 }
 Person.prototype = {
@@ -12,7 +12,7 @@ Person.prototype = {
     sleep() {
         console.log("Person is sleeping...")
     }
- }
+}
 
 // const hasan = Person("Hasan", 25);
 // hasan.sleep()
@@ -23,7 +23,7 @@ Person.prototype = {
 function Person1(name, age) {
     // let person = Object.create(Person.prototype)
     this.name = name;
-    this.age= age;
+    this.age = age;
     // return person;
 }
 Person1.prototype = {
@@ -33,7 +33,25 @@ Person1.prototype = {
     sleep() {
         console.log("Person is sleeping...")
     }
- }
+}
 
 const hasan1 = new Person1("Hasan", 25);
 // hasan1.sleep()
+
+
+// Use Class 
+
+class PersonwithClass{
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    eat() {
+        console.log("Person is eating...")
+    }
+    sleep() {
+        console.log("Person is sleeping...")
+    }
+}
+const tamim = new PersonwithClass("Tamim", 36)
+tamim.eat();
