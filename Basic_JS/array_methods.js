ea987b1e548ff82ac55dc805c3ac242f0d633a75
md-hasan-ali <hasanaliringku@gmail.com>
2024-02-18 12:44:48 +0600

    JS array method

---
+++ Basic_JS/array_methods.js
@@ -0,0 +1,20 @@
+// toString(), join() method 
+const array =  ['hasan', 'ringku', 'ali']
+console.log(array.toString())
+console.log(array.join(" * "))
+
+// splice method 
+// array.splice(1,1, 'Lemon', 'Alu')
+// console.log(array) 
+
+array.splice(0,1, 'Lemon', 'Alu')
+console.log(array)
+
+const array2 = ['payaj', 'potol']
+const array3 = ['payaj', 'potol']
+const mergeArray = array.concat(array2, array3)
+console.log(mergeArray)
+
+
+// Slice method 
+console.log(mergeArray.slice(1, 3))
